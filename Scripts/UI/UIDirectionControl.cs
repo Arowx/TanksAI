a84794e345cc904503a3ee9f4401acb714323449
Arowx <allan@arowx.com>
2015-11-06 20:15:24 +0000

    Challenge Manager Added
    The challenge manager plays each tank against every other.

--- Scripts/UI/UIDirectionControl.cs
+++ Scripts/UI/UIDirectionControl.cs
@@ -1,28 +1,22 @@
-﻿using UnityEngine;
+using UnityEngine;
 
-namespace Complete
+public class UIDirectionControl : MonoBehaviour
 {
-    public class UIDirectionControl : MonoBehaviour
+    public bool m_UseRelativeRotation = true;  
+
+
+    private Quaternion m_RelativeRotation;     
+
+
+    private void Start()
     {
-        // This class is used to make sure world space UI
-        // elements such as the health bar face the correct direction.
-
-        public bool m_UseRelativeRotation = true;       // Use relative rotation should be used for this gameobject?
+        m_RelativeRotation = transform.parent.localRotation;
+    }
 
 
-        private Quaternion m_RelativeRotation;          // The local rotatation at the start of the scene.
-
-
-        private void Start ()
-        {
-            m_RelativeRotation = transform.parent.localRotation;
-        }
-
-
-        private void Update ()
-        {
-            if (m_UseRelativeRotation)
-                transform.rotation = m_RelativeRotation;
-        }
+    private void Update()
+    {
+        if (m_UseRelativeRotation)
+            transform.rotation = m_RelativeRotation;
     }
 }
